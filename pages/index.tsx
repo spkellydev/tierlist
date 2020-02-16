801efbbbb9ae5824897497390957cef491dd1fe7
Sean Kelly <spkellydev@gmail.com>
2020-02-15 22:43:11 -0500

    server init

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+﻿import { NextPage } from 'next';
+import Link from 'next/link';
+
+const Home: NextPage<{ userAgent: string }> = ({ userAgent }) => (
+  <div>
+    <h1>Hello world! - user agent: {userAgent}</h1>
+    <li><Link href='/a' as='/a'><a>a</a></Link></li>
+    <li><Link href='/b' as='/b'><a>b</a></Link></li>
+  </div>
+);
+
+Home.getInitialProps = async ({ req }) => {
+  const userAgent = req ? req.headers['user-agent'] || '' : navigator.userAgent;
+  return { userAgent };
+};
+
+export default Home;
